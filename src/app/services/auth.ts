7897c3f9101e296abd38324d0ddbe25e2b1019c2
Squawks1 <to.toledom@duocuc.cl>
2025-11-30 16:28:23 -0300

    Update auth.ts
    Se declaran los localStorage dependiendo del estado de la sesión

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -3,6 +3,30 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class Auth {
-  
+export class AuthService {
+
+  private KEY = 'usuarioLogeado';
+
+  constructor() { }
+
+  // Dejar al usuario como logeado
+  login(usuarioId: number) {
+    localStorage.setItem(this.KEY, usuarioId.toString());
+  }
+
+  // Cerrar la sesión
+  logout() {
+    localStorage.removeItem(this.KEY);
+  }
+
+  // Ver si el usuario sigue conectado
+  isLoggedIn(): boolean {
+    return localStorage.getItem(this.KEY) !== null;
+  }
+
+  // Obtener ID de usuario
+  getUserId(): number | null {
+    const id = localStorage.getItem(this.KEY);
+    return id ? Number(id) : null;
+  }
 }
