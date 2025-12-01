28cf695ec6d0222053a980139b75abb4f6564f84
Squawks1 <to.toledom@duocuc.cl>
2025-11-30 22:44:32 -0300

    Update home.page.ts
    Se añaden métodos e imports para poder consumir la api-demo. Se presume que estará consumiendo constantemente, por lo que queda pendiente la optimización

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 
+import { ApiService } from '../services/api';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -12,7 +14,10 @@ export class HomePage implements OnInit{
   usuarioEmail: string = 'Invitado';
   MediReminder: string = 'MediReminder';
 
-  constructor(private router: Router, private menu: MenuController) {
+  // Variable para guardar respuesta de la API
+  posts: any[] = [];
+
+  constructor(private router: Router, private menu: MenuController, private api: ApiService) {
     const navigation = this.router.getCurrentNavigation();
     //Validación para entrar con un usuario o invitado
     this.usuarioEmail = navigation?.extras.state?.['user'] || 'Invitado';
@@ -20,7 +25,22 @@ export class HomePage implements OnInit{
 
   // Cerrar el menú lateral
   ngOnInit(): void {
-    this.menu.close("mainMenu")
+    this.menu.close("mainMenu");
+
+    // Llamar a la API
+    this.obtenerPosts();
   }
 
+  // Función para consumir la API
+  obtenerPosts() {
+    this.api.getPosts().subscribe({
+      next: (response: any) => {
+        console.log("Datos recibidos:", response);
+        this.posts = response;
+      },
+      error: (error) => {
+        console.error("Error al consumir API:", error);
+      }
+    });
+  }
 }
