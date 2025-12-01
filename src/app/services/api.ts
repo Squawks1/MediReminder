37ff8a614b7f10f14ec01c966e5a7ff0937b55cf
Squawks1 <to.toledom@duocuc.cl>
2025-11-30 22:01:25 -0300

    Update api.ts
    Se edita para consumir la api de ejemplo typicode, a falta de una propia

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Api {
-  
+export class ApiService {
+
+  // API de ejemplo typicode
+  private baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/posts`);
+  }
 }
