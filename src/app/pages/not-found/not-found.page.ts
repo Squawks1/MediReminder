a7afddb0d418703aae69a55c50fc46d4011f70b0
Squawks1 <to.toledom@duocuc.cl>
2025-11-30 23:15:01 -0300

    Update not-found.page.ts
    Se agrega standalone: false pendiente

--- src/app/pages/not-found/not-found.page.ts
+++ src/app/pages/not-found/not-found.page.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-not-found',
   templateUrl: './not-found.page.html',
   styleUrls: ['./not-found.page.scss'],
+  standalone: false
 })
 export class NotFoundPage implements OnInit {
 
