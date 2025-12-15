b29fa65eeb0e6d869a7613842eca9f2ace2ce897
Squawks1 <to.toledom@duocuc.cl>
2025-12-14 23:35:28 -0300

    Update login.cy.ts
    Se crean las validaciones para pruebas e2e

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,48 @@
+describe('Login MediReminder', () => {
+
+    beforeEach(() => {
+        cy.visit('/login');
+    });
+
+    it('Debe mostrar el formulario de login', () => {
+        cy.contains('Login').should('exist');
+        cy.get('[data-cy=usuario]').should('exist');
+        cy.get('[data-cy=password]').should('exist');
+        cy.get('[data-cy=btn-login]').should('be.disabled');
+    });
+
+    it('Debe validar campos incorrectos', () => {
+        cy.get('[data-cy=usuario]').type('ab');
+        cy.get('[data-cy=password]').type('12');
+
+        cy.get('[data-cy=btn-login]').should('be.disabled');
+
+        cy.contains('El usuario debe tener entre 3 y 8 caracteres').should('exist');
+        cy.contains('La contraseña debe ser numérica de 4 dígitos').should('exist');
+    });
+
+    it('Debe mostrar alerta si el usuario no existe', () => {
+        cy.get('[data-cy=usuario]').type('usuario1');
+        cy.get('[data-cy=password]').type('1234');
+
+        cy.get('[data-cy=btn-login]').click();
+
+        cy.contains('Usuario o contraseña incorrectos').should('exist');
+    });
+
+    it('Debe iniciar sesión correctamente y navegar a Home', () => {
+        cy.get('[data-cy=usuario]').type('admin');
+        cy.get('[data-cy=password]').type('1234');
+
+        cy.get('[data-cy=btn-login]').click();
+
+        cy.url().should('include', '/home');
+        cy.contains('Hola').should('exist');
+    });
+
+    it('Debe navegar a la página de registro', () => {
+        cy.get('[data-cy=btn-registro]').click();
+        cy.url().should('include', '/registro');
+    });
+
+});
