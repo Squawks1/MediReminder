0e58e7fce7f679f98e4e2551d0534d82f2e3b549
Squawks1 <to.toledom@duocuc.cl>
2025-12-14 23:22:22 -0300

    Update cypress.config.ts
    Se configura enrutamiento de e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:8100",
+    supportFile: "cypress/support/e2e.ts",
+    // specPattern: 'cypress/e2e/**/*/.{js,jsx,ys,tsx}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+
+    }
   },
 });
