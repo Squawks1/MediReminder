52bd4151f12eda6a5fa0e6bdaf13ce0fa22e24fd
Squawks1 <to.toledom@duocuc.cl>
2025-12-15 22:44:35 -0300

    Update api-connect.page.ts
    Se detallan los métodos para consumir la api

--- src/app/api-connect/api-connect.page.ts
+++ src/app/api-connect/api-connect.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api';
 
 @Component({
   selector: 'app-api-connect',
   templateUrl: './api-connect.page.html',
-  styleUrls: ['./api-connect.page.scss'],
+  standalone: false,
 })
 export class ApiConnectPage implements OnInit {
 
-  constructor() { }
+  posts: any[] = [];
+  cargando = false;
+  error = false;
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
+    this.obtenerPosts();
   }
 
+  obtenerPosts() {
+    this.cargando = true;
+
+    this.apiService.getPosts().subscribe({
+      next: (data) => {
+        this.posts = data;
+        this.cargando = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.error = true;
+        this.cargando = false;
+      }
+    });
+  }
 }
