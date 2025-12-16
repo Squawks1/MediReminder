06e937747cac6ec052ac8e370bb44f65d054253c
Squawks1 <to.toledom@duocuc.cl>
2025-12-15 21:58:02 -0300

    Update registro.cy.ts
    Se actualiza el detalle para pruebas en la page registro

---
+++ cypress/e2e/registro.cy.ts
@@ -0,0 +1,38 @@
+describe('Registro MediReminder', () => {
+
+    beforeEach(() => {
+        cy.visit('/registro');
+    });
+
+    it('Debe tener el botón Crear cuenta deshabilitado inicialmente', () => {
+        cy.get('[data-cy=btn-crear]').should('be.disabled');
+    });
+
+    it('Debe validar email incorrecto', () => {
+        cy.get('[data-cy=nombre]').type('Juan Perez');
+        cy.get('[data-cy=usuario]').type('juan1');
+        cy.get('[data-cy=email]').type('correo-invalido');
+        cy.get('[data-cy=password]').type('1234');
+        cy.get('[data-cy=confirmarPassword]').type('1234');
+
+        cy.contains('Ingrese un correo electrónico válido')
+        .should('exist');
+    });
+
+    it('Debe validar contraseñas no coincidentes', () => {
+        cy.get('[data-cy=nombre]').type('Juan Perez');
+        cy.get('[data-cy=usuario]').type('juan1');
+        cy.get('[data-cy=email]').type('juan@mail.com');
+        cy.get('[data-cy=password]').type('1234');
+        cy.get('[data-cy=confirmarPassword]').type('0000');
+
+        cy.contains('Las contraseñas no coinciden')
+        .should('exist');
+    });
+
+    it('Debe navegar al login', () => {
+        cy.get('[data-cy=btn-login]').click();
+        cy.url().should('include', '/login');
+    });
+
+});
