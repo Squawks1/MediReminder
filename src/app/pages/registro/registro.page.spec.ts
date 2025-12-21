a52a2b61f01d64f0f983e0f979b7a91294c457cd
Squawks1 <to.toledom@duocuc.cl>
2025-12-21 18:26:40 -0300

    Update registro.page.spec.ts

--- src/app/pages/registro/registro.page.spec.ts
+++ src/app/pages/registro/registro.page.spec.ts
@@ -1,11 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegistroPage } from './registro.page';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { IonicModule, Platform, ToastController } from '@ionic/angular';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApiService } from 'src/app/services/api';
+
+const sqliteMock = {
+  create: jasmine.createSpy('create').and.returnValue(Promise.resolve({}))
+};
+
+const platformMock = {
+  ready: () => Promise.resolve(),
+  backButton: {
+    subscribeWithPriority: jasmine.createSpy('subscribeWithPriority')
+  }
+};
+
+
+const toastControllerMock = {
+  create: jasmine.createSpy('create').and.returnValue(
+    Promise.resolve({ present: () => {} })
+  )
+};
+
+const apiServiceMock = {
+  get: jasmine.createSpy('get'),
+  post: jasmine.createSpy('post'),
+  login: jasmine.createSpy('login')
+};
 
 describe('RegistroPage', () => {
   let component: RegistroPage;
   let fixture: ComponentFixture<RegistroPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegistroPage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: SQLite, useValue: sqliteMock },
+        { provide: Platform, useValue: platformMock },
+        { provide: ToastController, useValue: toastControllerMock },
+        { provide: ApiService, useValue: apiServiceMock }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegistroPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
