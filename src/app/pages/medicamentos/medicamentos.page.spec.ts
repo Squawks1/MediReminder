5201247ebc31761b008ee2a6ebe2cb492833e83b
Squawks1 <to.toledom@duocuc.cl>
2025-12-21 18:26:45 -0300

    Update medicamentos.page.spec.ts

--- src/app/pages/medicamentos/medicamentos.page.spec.ts
+++ src/app/pages/medicamentos/medicamentos.page.spec.ts
@@ -1,11 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MedicamentosPage } from './medicamentos.page';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { IonicModule, Platform, ToastController } from '@ionic/angular';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApiService } from 'src/app/services/api';
+import { of } from 'rxjs';
+
+const sqliteMock = {
+  create: jasmine.createSpy('create').and.returnValue(Promise.resolve({}))
+};
+
+const platformMock = {
+  ready: () => Promise.resolve(),
+  backButton: {
+    subscribeWithPriority: jasmine.createSpy('subscribeWithPriority')
+  }
+};
+
+
+const toastControllerMock = {
+  create: jasmine.createSpy('create').and.returnValue(
+    Promise.resolve({ present: () => {} })
+  )
+};
+
+const apiServiceMock = {
+  getPosts: jasmine.createSpy('getPosts').and.returnValue(of([])),
+  get: jasmine.createSpy('get').and.returnValue(of({})),
+  post: jasmine.createSpy('post').and.returnValue(of({})),
+  login: jasmine.createSpy('login').and.returnValue(of({}))
+};
 
 describe('MedicamentosPage', () => {
   let component: MedicamentosPage;
   let fixture: ComponentFixture<MedicamentosPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MedicamentosPage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: SQLite, useValue: sqliteMock },
+        { provide: Platform, useValue: platformMock },
+        { provide: ToastController, useValue: toastControllerMock },
+        { provide: ApiService, useValue: apiServiceMock }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(MedicamentosPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
