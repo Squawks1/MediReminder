90f30db4d8163755e17e6e4ed47a6db75ddb0e31
Squawks1 <to.toledom@duocuc.cl>
2025-12-21 18:26:52 -0300

    Update home.page.spec.ts

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,7 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
 import { HomePage } from './home.page';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { IonicModule, Platform, ToastController } from '@ionic/angular';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApiService } from 'src/app/services/api';
+import { HomePageRoutingModule } from './home-routing.module';
+import { of } from 'rxjs';
+
+const sqliteMock = {
+  create: jasmine.createSpy('create').and.returnValue(Promise.resolve({}))
+};
+
+const platformMock = {
+  ready: () => Promise.resolve(),
+  backButton: {
+    subscribeWithPriority: jasmine.createSpy('subscribeWithPriority')
+  }
+};
+
+
+const toastControllerMock = {
+  create: jasmine.createSpy('create').and.returnValue(
+    Promise.resolve({ present: () => {} })
+  )
+};
+
+const apiServiceMock = {
+  getPosts: jasmine.createSpy('getPosts').and.returnValue(of([])),
+  get: jasmine.createSpy('get').and.returnValue(of({})),
+  post: jasmine.createSpy('post').and.returnValue(of({})),
+  login: jasmine.createSpy('login').and.returnValue(of({}))
+};
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -10,7 +39,13 @@ describe('HomePage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        { provide: SQLite, useValue: sqliteMock },
+        { provide: Platform, useValue: platformMock },
+        { provide: ToastController, useValue: toastControllerMock },
+        { provide: ApiService, useValue: apiServiceMock }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
