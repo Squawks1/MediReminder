820bda7775b7658ff7ae34b16a193c9398bcf717
Squawks1 <to.toledom@duocuc.cl>
2025-12-21 18:26:27 -0300

    Update dbservice.spec.ts

--- src/app/services/dbservice.spec.ts
+++ src/app/services/dbservice.spec.ts
@@ -1,12 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
 import { Dbservice } from './dbservice';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Platform, ToastController } from '@ionic/angular';
+
+const sqliteMock = {
+  create: jasmine.createSpy('create').and.returnValue(
+    Promise.resolve({})
+  )
+};
+
+const platformMock = {
+  ready: () => Promise.resolve()
+};
+
+const toastControllerMock = {
+  create: jasmine.createSpy('create').and.returnValue(
+    Promise.resolve({
+      present: jasmine.createSpy('present')
+    })
+  )
+};
 
 describe('Dbservice', () => {
   let service: Dbservice;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        Dbservice,
+        { provide: SQLite, useValue: sqliteMock },
+        { provide: Platform, useValue: platformMock },
+        { provide: ToastController, useValue: toastControllerMock }
+      ]
+    });
+
     service = TestBed.inject(Dbservice);
   });
 
